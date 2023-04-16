423ddf411a0b707e4c99edb8276e0fe8847db759
Anderson Martinez <vsacavgc@gmail.com>
2023-04-16 13:09:51 -0500

    Adicion de la funcionalidad para desencriptar un texto

---
+++ index.js
@@ -0,0 +1,49 @@
+//condiciones
+//a - ai
+//e - enter
+//i - imes
+//o - ober
+//u - ufat
+
+function encriptar() {
+    var texto = document.getElementById("inputtexto").value.toLowerCase();
+    // i es para que afecte tanto mayuscula como minuscula
+    //g es para toda la liena u oracion
+    //s para que afecte a multiples lineas o parrafos
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar() {
+    var texto = document.getElementById("inputtexto").value.toLowerCase();
+    // i es para que afecte tanto mayuscula como minuscula
+    //g es para toda la liena u oracion
+    //s para que afecte a multiples lineas o parrafos
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function copiar(){
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("¡se copio!");
+}
